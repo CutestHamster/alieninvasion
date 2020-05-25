1663c5966888fdabdf2de4fe99505abf5735fe5f
CutestHamster <65850137+CutestHamster@users.noreply.github.com>
2020-05-25 06:16:17 -0400

    Added bullet system

--- ship.py
+++ ship.py
@@ -11,13 +11,13 @@ class Ship(object):
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
-        self.image_rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
-        self.image_rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
         # Store a decimal value for the ship's horizontal position
-        self.x = float(self.image_rect.x)
+        self.x = float(self.rect.x)
 
         # Movement flags
         self.moving_right = False
@@ -26,14 +26,14 @@ class Ship(object):
     def update(self):
         """Update the ship's position based on the movement flags"""
         # Update the ship's x value, not the rect
-        if self.moving_right and self.image_rect.right < self.screen_rect.right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.image_rect.left > 0:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x
-        self.image_rect.x = self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
-        self.screen.blit(self.image, self.image_rect)
+        self.screen.blit(self.image, self.rect)
